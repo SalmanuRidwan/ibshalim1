1d3976443d70230b5b75a8378ae9832a9f6d6a43
Ridwan Salmanu <ridwan.salmanu96@yahoo.com>
2023-11-07 21:09:12 +0100

    set up trusted origin and integrated email api

--- ibshalim/settings.py
+++ ibshalim/settings.py
@@ -149,4 +149,20 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# Email Settings
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+
+# CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST = (
+#     'http://localhost:7000',
+#     'https://4495-105-113-24-85.ngrok-free.app'
+# )
+
+CSRF_TRUSTED_ORIGINS = ['https://4495-105-113-24-85.ngrok-free.app']
